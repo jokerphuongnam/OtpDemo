4c524bcd3d2b21f6b2f05256112b67834c0d2a09
Phương Nam <nampp@mapmaker.asia>
2022-05-10 15:34:13 +0700

    Initial Commit

---
+++ OtpDemo/OtpDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtpDemoApp.swift
+//  OtpDemo
+//
+//  Created by gannha on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct OtpDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
